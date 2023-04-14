936bf1e8431b24c1fee8114be666d350178312b9
Desy Fitriani <desyfitriani234@gmail.com>
2023-04-14 22:27:20 +0700

    finish

--- ui.js
+++ ui.js
@@ -7,6 +7,10 @@ import puppeteer from "puppeteer";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const searchQuery = "Apply for a number plate";
+    const locQuery = {
+      query: "Sydney Domestic Airport 2020",
+      result: "Marrickville Service Centre",
+    };
     const baseUrl = "https://www.service.nsw.gov.au";
 
     await page.goto(baseUrl);
@@ -17,7 +21,10 @@ import puppeteer from "puppeteer";
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type('input[aria-controls^="homeautosuggestDXr"]', searchQuery);
+    const searchElement = await page.waitForSelector(
+      'input[aria-controls^="homeautosuggestKcpXb"]'
+    );
+    await searchElement.type(searchQuery);
     // await page.click('div[id="homeautosuggestDXr-hmpYxc4"]+div>button');
     await page.keyboard.press("Enter");
     const searchResults = await page.waitForSelector(".search__result");
@@ -26,19 +33,18 @@ import puppeteer from "puppeteer";
     const params = qs.stringify({ q: searchQuery }, { format: "RFC1738" });
     const searchUrl = `${baseUrl}/search-results?${params}`;
 
-    console.log(page.url());
-    console.log(searchUrl);
-    console.log(searchResults);
+    console.log("actual URL: ",page.url());
+    console.log("expected URL: ",searchUrl);
 
     assert.equal(page.url(), searchUrl);
     await page.goto(baseUrl);
-    await page.waitForSelector("text/Find Locations");
-    await page.click("text/Find Locations");
-    const searchElement = await page.waitForSelector("#locatorTextSearch");
-    await searchElement.type("Sydney 2000");
+    await page.waitForSelector("text/Find locations");
+    await page.click("text/Find locations");
+    const locationElement = await page.waitForSelector("#locatorTextSearch");
+    await locationElement.type(locQuery.query);
     await page.keyboard.press("Enter");
     // const locationResults = await page.waitForSelector(".location__title");
-    await page.waitForSelector("text/Marrickville Service Centre");
+    await page.waitForSelector(`text/${locQuery.result}`);
 
     await browser.close();
   } catch (error) {
